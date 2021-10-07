e6b0436c6884ba8119c0b0cb7a7aef34ebf0ab1e
tsukimidori <kohei.mizoue17@gmail.com>
2021-10-07 15:34:43 +0900

    quest及びplaceとtarget_attributeモデルの作成

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,2 +1,12 @@
 class Quest < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :title,               presence: true
+  validates :date,                presence: true
+  validates :target,              presence: true
+  validates :place_id,            presence: true
+  validates :target_attribute_id, presence: true
+
+
+  belongs_to :place
+  belongs_to :target_attribute
 end
