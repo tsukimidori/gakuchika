88f0e146bec734431e6048fc14dd4488db544583
tsukimidori <kohei.mizoue17@gmail.com>
2021-10-12 12:36:40 +0900

    マイページに実績を追加 他

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @quests = Quest.where(user_id: params[:id])
+    @joins = Join.where(user_id: params[:id])
   end
 end
